baf9495c02f99b16c062e905918939f4c27d49e0
Alec <aroarks1@gmail.com>
2023-02-13 21:15:13 -0500

    Add function to play a single round of rock paper scissors using outputs of player and computer choice functions

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -1,12 +1,14 @@
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
         computerChoice = "rock";
         return computerChoice;
-    } else if (computerChoice === 1) {
+    } 
+    else if (computerChoice === 1) {
         computerChoice = "paper";
         return computerChoice;
-    } else {
+    } 
+    else {
         computerChoice = "scissors";
         return computerChoice;
     }
@@ -14,10 +16,27 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     let playerChoice = prompt("Please choose rock, paper, or scissors: ");
-    playerChoice = playerChoice.toLowerCase()
+    playerChoice = playerChoice.toLowerCase();
     if (playerChoice === ("rock") || ("paper") ||  ("scissors")) {
         return playerChoice;
-    } else {
+    } 
+    else {
         return "Invalid entry.";
+    }
+}
+
+function playRound () {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice) {
+        return `You both chose ${playerChoice}. It's a draw!`;
+    } 
+    else if ((playerChoice === "rock" && computerChoice === "scissors") 
+    || (playerChoice === "paper" && computerChoice === "rock") 
+    || (playerChoice === "scissors" && computerChoice === "paper")) {
+        return `You chose ${playerChoice}, while your opponent chose ${computerChoice}. You win!`;
+    } 
+    else {
+        return `You chose ${playerChoice}, while your opponent chose ${computerChoice}. You lose...`;
     }
 }
