ab313451f1712153cdf0fd68cd525dfef4f0f488
Alec <aroarks1@gmail.com>
2023-02-13 23:58:36 -0500

    Combine single-round play function and player choice function, add final looping game function

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -14,29 +14,41 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
+function playRound() {
     let playerChoice = prompt("Please choose rock, paper, or scissors: ");
+    const computerChoice = getComputerChoice();
     playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === ("rock") || ("paper") ||  ("scissors")) {
-        return playerChoice;
-    } 
+    if (playerChoice === computerChoice) {
+        console.log(`You both chose ${playerChoice}. The round is a draw!`);
+    }
+    else if (((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper"))) {
+        console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`);
+        return true;
+    }
     else {
-        return "Invalid entry.";
+        console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You lost the round...`);
+        return false;
     }
 }
 
-function playRound () {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    if (playerChoice === computerChoice) {
-        return `You both chose ${playerChoice}. It's a draw!`;
-    } 
-    else if ((playerChoice === "rock" && computerChoice === "scissors") 
-    || (playerChoice === "paper" && computerChoice === "rock") 
-    || (playerChoice === "scissors" && computerChoice === "paper")) {
-        return `You chose ${playerChoice}, while your opponent chose ${computerChoice}. You win!`;
-    } 
-    else {
-        return `You chose ${playerChoice}, while your opponent chose ${computerChoice}. You lose...`;
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let victMessage = undefined;
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound();
+        if (roundResult === true) {
+        playerScore = playerScore + 1;
+        }
+        else if (roundResult === false) {
+        computerScore = computerScore + 1;
+        }
     }
+    if (playerScore > computerScore) {
+        victMessage = "You won. Congratulations!"
+    }
+    else {
+        victMessage = "You lost. Too bad!"
+    }
+    console.log(`You won ${playerScore} rounds while your opponent won ${computerScore} rounds. ${victMessage}`);
 }
