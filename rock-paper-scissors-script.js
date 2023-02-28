dd69921ea86826a26c756f3c28cd49af653fec87
Alec <aroarks1@gmail.com>
2023-02-28 01:33:59 -0500

    Redo code with switch statements, clean up code

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -1,27 +1,23 @@
-function getComputerChoice() {
+const getComputerChoice = function() {
     let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        computerChoice = "rock";
-        return computerChoice;
-    } 
-    else if (computerChoice === 1) {
-        computerChoice = "paper";
-        return computerChoice;
-    } 
-    else {
-        computerChoice = "scissors";
-        return computerChoice;
-    }
-}
+    switch(computerChoice) {
+        case 0:
+            return computerChoice = 'rock';
+        case 1:
+            return computerChoice = 'paper';
+        case 2:
+            return computerChoice = 'scissors';
+        }
+};
 
-function playRound() {
-    let playerChoice = prompt("Please choose rock, paper, or scissors: ");
+const playRound = function() {
+    let playerChoice = prompt('Please choose rock, paper, or scissors: ');
     const computerChoice = getComputerChoice();
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
         console.log(`You both chose ${playerChoice}. The round is a draw!`);
     }
-    else if (((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper"))) {
+    else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
         console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`);
         return true;
     }
@@ -29,26 +25,29 @@ function playRound() {
         console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You lost the round...`);
         return false;
     }
-}
+};
 
-function game() {
+const runGame = function() {
     let playerScore = 0;
     let computerScore = 0;
-    let victMessage;
+    let endMessage = "";
     for (let i = 0; i < 5; i++) {
-        let roundResult = playRound();
-        if (roundResult === true) {
-        playerScore = playerScore + 1;
+        switch(playRound()) {
+            case true:
+                playerScore++;
+                break;
+            case false:
+                computerScore++;
+                break;
         }
-        else if (roundResult === false) {
-        computerScore = computerScore + 1;
-        }
     }
-    if (playerScore > computerScore) {
-        victMessage = "You won. Congratulations!";
+    switch (playerScore > computerScore) {
+        case true:
+            endMessage = 'You won! Congratulations!';
+            break;
+        case false:
+            endMessage = 'You lost. Too bad!';
+            break;
     }
-    else {
-        victMessage = "You lost. Too bad!";
-    }
-    console.log(`You won ${playerScore} rounds while your opponent won ${computerScore} rounds. ${victMessage}`);
-}
+    console.log(`You won ${playerScore} games while your oppnent won ${computerScore}. ${endMessage}`);
+};
