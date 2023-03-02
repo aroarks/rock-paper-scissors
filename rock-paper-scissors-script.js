8272bd4ad03978a72d47ff7b34cd705c2b4a42d5
Alec <aroarks1@gmail.com>
2023-03-01 20:54:44 -0500

    Add basic user interface, comment out runGame logic for now

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -1,4 +1,4 @@
-const getComputerChoice = function() {
+const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
     switch(computerChoice) {
         case 0:
@@ -10,44 +10,59 @@ const getComputerChoice = function() {
         }
 };
 
-const playRound = function() {
-    let playerChoice = prompt('Please choose rock, paper, or scissors: ');
+const playRound = playerChoice => {
     const computerChoice = getComputerChoice();
-    playerChoice = playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
-        console.log(`You both chose ${playerChoice}. The round is a draw!`);
+        resultSpan.textContent = `you both chose ${playerChoice}. The round is a draw!`;
     }
     else if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`);
-        return true;
+        resultSpan.textContent = `you chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`;
+        return playerScore++;
     }
     else {
-        console.log(`You chose ${playerChoice} while your opponent chose ${computerChoice}. You lost the round...`);
-        return false;
+        resultSpan.textContent = `you chose ${playerChoice} while your opponent chose ${computerChoice}. You lost the round...`;
+        return computerScore++;
     }
 };
 
-const runGame = function() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let endMessage = "";
-    for (let i = 0; i < 5; i++) {
-        switch(playRound()) {
-            case true:
-                playerScore++;
-                break;
-            case false:
-                computerScore++;
-                break;
-        }
-    }
-    switch (playerScore > computerScore) {
-        case true:
-            endMessage = 'You won! Congratulations!';
-            break;
-        case false:
-            endMessage = 'You lost. Too bad!';
-            break;
-    }
-    console.log(`You won ${playerScore} games while your oppnent won ${computerScore}. ${endMessage}`);
-};
+let resultSpan = document.querySelector('#resultSpan')
+let buttonOne = document.querySelector('#buttonOne');
+let buttonTwo = document.querySelector('#buttonTwo');
+let buttonThree = document.querySelector('#buttonThree');
+
+buttonOne.addEventListener('click', () => {
+    playRound('rock');
+});
+
+buttonTwo.addEventListener('click', () => {
+    playRound('paper');
+});
+
+buttonThree.addEventListener('click', () => {
+    playRound('scissors');
+});
+
+// const runGame = function() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let endMessage = "";
+//     for (let i = 0; i < 5; i++) {
+//         switch(playRound()) {
+//             case true:
+//                 playerScore++;
+//                 break;
+//             case false:
+//                 computerScore++;
+//                 break;
+//         }
+//     }
+//     switch (playerScore > computerScore) {
+//         case true:
+//             endMessage = 'You won! Congratulations!';
+//             break;
+//         case false:
+//             endMessage = 'You lost. Too bad!';
+//             break;
+//     }
+//     console.log(`You won ${playerScore} games while your oppnent won ${computerScore}. ${endMessage}`);
+// };
