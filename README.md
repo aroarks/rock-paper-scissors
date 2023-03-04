7fc67c53b16fba5ad7425c0d957fe995ec3e1f2f
Alec <aroarks1@gmail.com>
2023-03-03 20:51:09 -0500

    Redo UI

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 main container (score info, round result, game buttons)
 
 score info (player score, oppontent score)
+
+main container
+    text container
+        player score
+        opponent score
+        round result
+    button container
+        rock button
+        paper button
+        scissors button
--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -10,31 +10,32 @@ const getComputerChoice = function() {
 };
 
 const playRound = function(playerChoice) {
-    const computerChoice = getComputerChoice()
+    const computerChoice = getComputerChoice();
     if (playerChoice === computerChoice) {
-        resultInfo.textContent = `You both chose ${playerChoice}. The round is a draw!`;
+        resultInfoDisplay.textContent = `You both chose ${playerChoice}. The round is a draw!`;
     }
     else if ((playerChoice === 'rock' && computerChoice === 'scissors') 
     || (playerChoice === 'paper' && computerChoice === 'rock') 
     || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        resultInfo.textContent = `You chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`;
+        resultInfoDisplay.textContent = `You chose ${playerChoice} while your opponent chose ${computerChoice}. You win the round!`;
         playerScore++;
     }
     else {
-        resultInfo.textContent = `You chose ${playerChoice} while your opponent chose ${computerChoice}. You lose the round...`;
+        resultInfoDisplay.textContent = `You chose ${playerChoice} while your opponent chose ${computerChoice}. You lose the round.`
         computerScore++;
     }
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 };
 
-const buttons = gameButtons.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playRound(button.id);
-        scoreInfo.textContent = `Your Score: ${playerScore}   Opponent Score: ${computerScore}`;
-    });
-});
-
-const scoreInfo = document.querySelector('#scoreInfo');
-const resultInfo = document.querySelector('#resultInfo');
 let playerScore = 0;
 let computerScore = 0;
+const playerScoreDisplay = document.querySelector('#playerScore');
+const computerScoreDisplay = document.querySelector('#computerScore');
+const resultInfoDisplay = document.querySelector('.roundReadout');
+const gameButtons = document.querySelectorAll('button.gameButton');
+gameButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
